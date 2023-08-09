175f996ebbf4b47796e18bfb7f8cb1add1b861b6
Tjian Lam <tjian.lam@bam.com>
2023-08-09 16:36:19 +0200

    change way to calculate polling interval based on X-RateLimit-Limit instead

--- ingestor/src/ingestion.py
+++ ingestor/src/ingestion.py
@@ -16,6 +16,7 @@ def poll_github_events():
 
     if github_token := settings.github_token:
         headers["Authorization"] = f"Bearer {github_token}"
+        logging.info("Polling Github Events API with token, requests limited to 5000 per hour")
     else:
         logging.info("Polling Github Events API without token, requests limited to 60 per hour")
 
@@ -23,14 +24,16 @@ def poll_github_events():
         first_page: FirstPage = get_first_page(url=url, headers=headers)
         if first_page.status_code == 200:
             events.extend(service.parse_events_redis(first_page.events))
-            events = get_paginated_data(response=first_page, headers=headers, events=events)
+            events: list[dict] = get_paginated_data(response=first_page, headers=headers, events=events)
             yield events
 
             # Preparing for next poll
             events.clear()
             headers["If-None-Match"] = first_page.etag
             time.sleep(
-                max(first_page.poll_interval * first_page.total_pages, settings.max_poll_interval)  # Poll dependent on the poll interval limit
+                max(
+                    first_page.poll_interval * first_page.total_pages, settings.max_poll_interval
+                )  # Poll dependent on the poll interval limit
             )
         else:
             logging.info("Trying again in 60 seconds")
@@ -44,7 +47,7 @@ def get_first_page(url: str, headers: dict) -> FirstPage:
             return FirstPage(
                 status_code=response.status_code,
                 events=response.json(),
-                poll_interval=int(response.headers["X-Poll-Interval"]),
+                poll_interval=round(3600 / int(response.headers["X-RateLimit-Limit"])),
                 etag=response.headers["ETag"],
                 next=response.links.get("next")["url"],
                 total_pages=response.links.get("last")["url"][-1],
@@ -60,14 +63,14 @@ def get_first_page(url: str, headers: dict) -> FirstPage:
         logging.error(f"Failed to connect: {e}")
 
 
-def get_paginated_data(response: FirstPage, headers: dict, events: list) -> list:
+def get_paginated_data(response: FirstPage, headers: dict, events: list[dict]) -> list[dict]:
     if response.next:
-        next_page_url = response.next
+        next_page_url: str = response.next
         while next_page_url:
             response: requests.models.Response = requests.get(next_page_url, headers=headers)
             if response.status_code == 200:
                 events.extend(response.json())
-                next_page_url = response.links["next"]["url"] if response.links.get("next") else None
+                next_page_url: str | None = response.links["next"]["url"] if response.links.get("next") else None
             elif response.reason == "rate limit exceeded":
                 logging.error("Github Events API limit exceeded, writing collected events so far")
                 break
